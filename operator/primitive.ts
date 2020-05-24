29951517ea51b01256f668d1450014505c0f557d
Aditi Mohanty <rheaditi@users.noreply.github.com>
2020-05-24 16:51:28 +0530

    feat: add pick to PrimitiveOperator

--- operator/primitive.ts
+++ operator/primitive.ts
@@ -1,4 +1,5 @@
 import { BaseOperator } from './base.ts';
+import pick from "https://deno.land/x/lodash/pick.js";
 
 export class PrimitiveOperator<Op> extends BaseOperator<Op> {
   constructor(data: Op) {
@@ -13,5 +14,10 @@ export class PrimitiveOperator<Op> extends BaseOperator<Op> {
     updateMethod: (currentValue: Op) => T,
   ): PrimitiveOperator<ReturnType<typeof updateMethod>> {
     return new PrimitiveOperator<ReturnType<typeof updateMethod>>(updateMethod(this.data));
+  }
+
+  pick<U extends keyof Op>(paths: Array<U>) {
+    const picked = pick(this.data, ...paths) as { [P in U]: Op[P]; };
+    return new PrimitiveOperator<{ [P in U]: Op[P]; }>(picked);
   }
 }
--- tests/PrimitiveOperator.test.ts
+++ tests/PrimitiveOperator.test.ts
@@ -1,5 +1,7 @@
-import { PrimitiveOperator } from "../operator/operators.ts";
 import { assertEquals } from "https://deno.land/std/testing/asserts.ts";
+
+import { PrimitiveOperator } from "../operator/operators.ts";
+import { Pokemon, pokemon } from '../data/pokemon.ts';
 
 Deno.test("PrimitiveOperator: instantiates with value", () => {
   const data = { id: "1234", name: "foo" };
@@ -24,5 +26,17 @@ Deno.test("PrimitiveOperator: updates a value", () => {
     {
       name: "Jane Doe",
     },
+  );
+});
+
+Deno.test('PrimitiveOperator: picks object keys', () => {
+  const rawPokemon = pokemon[0];
+
+  const op = new PrimitiveOperator<Pokemon>(rawPokemon);
+  const charredPokemon = op.pick(['name', 'weight']);
+
+  assertEquals(
+    charredPokemon.value(),
+    { name: rawPokemon.name, weight: rawPokemon.weight },
   );
 });
